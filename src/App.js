7bf1ccb26c11db81c98d02779a35e7ba820d0358
shapovalov-sasha <shapovalovalexandru@gmail.com>
2022-06-18 22:35:34 +0300

    removed first component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import "./App.css";
-import MyFirstComponent from "./components/MyFirstComponent";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <MyFirstComponent />
-      </header>
+      <div className="App-header"></div>
     </div>
   );
 }
