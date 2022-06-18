98ddc427cfde4850e7d3a3c5ebbb38981037c200
Sapovalov Alexandru <shapovalovalexandru@gmail.com>
2022-06-18 16:08:51 +0300

    initial setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello world!</h1>
+        <p>Start learning React !</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
