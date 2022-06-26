22302a46a82ec376d2cf329a7febde7555b916e2
shapovalov-sasha <shapovalovalexandru@gmail.com>
2022-06-26 21:59:41 +0300

    assingnment 1 - Task Viewer solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import TaskCard from "./components/task-card/TaskCard";
+import TaskViewer from "./components/task-viewer/TaskViewer";
 
 function App() {
   const data = [
@@ -21,28 +21,38 @@ function App() {
       status: "Complete",
       dueDate: new Date(2022, 6, 13),
     },
+    {
+      id: "T-1",
+      name: "Create a Design System for Enum Workspace.",
+      status: "Todo",
+      dueDate: new Date(2022, 5, 23),
+    },
+    {
+      id: "T-2",
+      name: "12 Create a Design System for Enum Workspace.",
+      status: "In Progress",
+      dueDate: new Date(2022, 7, 24),
+    },
+    {
+      id: "T-3",
+      name: "13 Create a Design System for Enum Workspace.",
+      status: "Complete",
+      dueDate: new Date(2022, 6, 13),
+    },
+    {
+      id: "T-2",
+      name: "12 Create a Design System for Enum Workspace.",
+      status: "In Progress",
+      dueDate: new Date(2022, 7, 24),
+    },
   ];
 
   return (
     <div className="app-container">
-      <TaskCard
-        id={data[0].id}
-        status={data[0].status}
-        name={data[0].name}
-        dueDate={data[0].dueDate}
-      />
-      <TaskCard
-        id={data[1].id}
-        status={data[1].status}
-        name={data[1].name}
-        dueDate={data[1].dueDate}
-      />
-      <TaskCard
-        id={data[2].id}
-        status={data[2].status}
-        name={data[2].name}
-        dueDate={data[2].dueDate}
-      />
+      <div className="app-content">
+        <h3>Task Manager</h3>
+        <TaskViewer taskList={data} />
+      </div>
     </div>
   );
 }
---
+++ src/components/task-viewer/TaskViewer.js
@@ -0,0 +1,22 @@
+import "./TaskViewer.css";
+import TaskCard from "../task-card/TaskCard";
+
+function TaskViewer(props) {
+  return (
+    <div className="task-viewer-container">
+      <div className="task-list-container">
+        {props.taskList.map((item, index) => (
+          <TaskCard
+            key={index}
+            id={item.id}
+            status={item.status}
+            name={item.name}
+            dueDate={item.dueDate}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default TaskViewer;
