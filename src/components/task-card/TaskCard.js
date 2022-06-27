d591d94057eb407b041c0408d1930061361f912d
shapovalov-sasha <shapovalovalexandru@gmail.com>
2022-06-27 05:27:37 +0300

    React events - onClick

--- src/components/task-card/TaskCard.js
+++ src/components/task-card/TaskCard.js
@@ -3,16 +3,24 @@ import Badge from "../badge/Badge";
 import DateContainer from "../date-container/DateContainer";
 
 function TaskCard(props) {
+  let taskId = props.id;
+
+  const handleClick = () => {
+    console.log("Clicked! " + props.id);
+    taskId = taskId + "Clicked !";
+  };
+
   return (
     <div className="card-wrapper">
       <div className="card-header">
-        <p className="task-id">{props.id}</p>
+        <p className="task-id">{taskId}</p>
         <Badge status={props.status} />
       </div>
       <div className="card-content">
         <p>{props.name}</p>
       </div>
       <div className="card-footer">
+        <button onClick={handleClick}>Click me!</button>
         <DateContainer date={props.dueDate} />
       </div>
     </div>
