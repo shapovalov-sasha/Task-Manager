83454726ad89c92a8224a65675e4caff8da1d7a3
shapovalov-sasha <shapovalovalexandru@gmail.com>
2022-06-30 02:00:10 +0300

    form with multiple states

--- src/components/forms/CreateTaskForm.jsx
+++ src/components/forms/CreateTaskForm.jsx
@@ -6,62 +6,20 @@ const CreateTaskForm = () => {
   const [dueDate, setDueDate] = useState();
   const [taskDetails, setTaskDetails] = useState("");
 
-  /* 
-  // State -> obiect, toate datele sunt salvate intr-un singur state.
-  const [formData, setFormData] = useState({
-    taskName: "",
-    dueDate: "",
-    taskDetails: "",
-  });
- */
-
-  /* 
-  // Functia care schimba state-ul de tip obiect pe baza numelui field-ului care a apelat-o
-  const handleInputChange = (event) => {
-    setFormData((prevState) => ({
-      ...prevState,
-      [event.target.name]: event.target.value,
-    }));
-  };
-  */
-
   const handleNameChange = (event) => {
-    /*   
-    setFormData((prevState) => ({
-      ...prevState,
-      taskName: event.target.value,
-    }));
- */
     setTaskName(event.target.value);
   };
 
   const handleDateChange = (event) => {
-    /*  
-    setFormData((prevState) => ({
-      ...prevState,
-      dueDate: event.target.value,
-    })); 
-    */
     setDueDate(event.target.value);
   };
 
   const handleDetailsChange = (event) => {
-    /*   
-    setFormData((prevState) => ({
-      ...prevState,
-      taskDetails: event.target.value,
-    }));
-     */
     setTaskDetails(event.target.value);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    /*
-    // decomentati daca utilizati state-ul de tip obiect,
-    // nu uitati sa comentati codul de la state-ul idividual in acest caz
-         console.log("formData", formData);
-    */
     const newTask = {
       name: taskName,
       dueDate: dueDate,
