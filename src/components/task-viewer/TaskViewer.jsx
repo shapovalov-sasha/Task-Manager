2aa7050f8c0a97218daf55897d3f8182d76ef8b6
shapovalov-sasha <shapovalovalexandru@gmail.com>
2022-07-03 19:13:50 +0300

    Moved Create Task Form inside Modal

--- src/components/task-viewer/TaskViewer.jsx
+++ src/components/task-viewer/TaskViewer.jsx
@@ -5,17 +5,15 @@ import TaskCard from "../task-card/TaskCard";
 function TaskViewer(props) {
   return (
     <div className="task-viewer-container">
-      {/* 
       <div className="control-panel-container">
         <div>
           <h3 className="view-title">Tasks</h3>
           <p className="subtitle">Your tasks in your space.</p>
         </div>
-        <button className="button-primary" type="submit">
+        <button onClick={props.onCreateClick} className="button-primary">
           Create Task
         </button>
-      </div> 
-      */}
+      </div>
 
       <div className="task-list-container">
         {props.taskList.map((item, index) => (
