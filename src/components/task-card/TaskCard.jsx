1f1e259611f81943bb2ecbac685f5a2c847bbf31
shapovalov-sasha <shapovalovalexandru@gmail.com>
2022-08-19 00:43:01 +0300

    Refactor on Badge component

--- src/components/task-card/TaskCard.jsx
+++ src/components/task-card/TaskCard.jsx
@@ -4,11 +4,25 @@ import Badge from "../badge/Badge";
 import DateContainer from "../date-container/DateContainer";
 
 function TaskCard(props) {
+  const getBadgeStyle = () => {
+    switch (props.status) {
+      case "Todo":
+        return "grey";
+      case "In Progress":
+        return "blue";
+      case "Completed":
+        return "green";
+      case "Pending":
+        return "orange";
+      default:
+        return "grey";
+    }
+  };
   return (
     <div className="card-wrapper">
       <div className="card-header">
         <p className="task-id">{props.id}</p>
-        <Badge status={props.status} />
+        <Badge label={props.status} color={getBadgeStyle()} />
       </div>
       <div className="card-content">
         <p>{props.name}</p>
